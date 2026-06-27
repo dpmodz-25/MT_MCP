56a76d02654c39eb776925999250d8b1fe8fcdcc
dpmodz <putradandiiskandar25@gmail.com>
2026-06-28 01:41:26 +0700

    Update main.py

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ from app import start_server
 start_server()
 
 # 1. Konfigurasi Kunci Akses (Gunakan Environment Variables)
-TELEGRAM_TOKEN = "8607503824:AAEvECrjkQo_GlJQ09_xVtohGMAjbSxOqas"
+TELEGRAM_TOKEN = "8607503824:AAHB4cVhia6jQm3TEnq0z22Q75syhtsbUXQ"
 GROQ_API_KEY = os.getenv("GROQ_API_KEY")
 
 # ID Pemilik Bot Anda
