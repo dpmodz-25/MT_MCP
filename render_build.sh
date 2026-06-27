bef914ab98d9efa333726a2d5e9e30cd307a3849
dpmodz <putradandiiskandar25@gmail.com>
2026-06-28 01:12:16 +0700

    Update render_build.sh

--- render_build.sh
+++ render_build.sh
@@ -5,16 +5,16 @@ set -o errexit
 # 1. Unduh dan instal Java Runtime Portabel (OpenJDK 17) secara lokal di folder Render
 echo "☕ Mengunduh Java Runtime Portabel untuk Render..."
 mkdir -p $HOME/java
-curl -sL https://github.com | tar -xz -C $HOME/java --strip-components=1
+curl -sL "https://github.com" | tar -xz -C $HOME/java --strip-components=1
 
 # 2. Masukkan Java ke PATH sistem Render secara permanen
 export PATH=$HOME/java/bin:$PATH
 
-# 3. Unduh biner Apktool terbaru
+# 3. Unduh biner Apktool terbaru dari repositori resmi
 echo "📦 Memasang Apktool lokal engine..."
 mkdir -p $HOME/bin
-curl -sL https://githubusercontent.com -o $HOME/bin/apktool
-curl -sL https://github.com -o $HOME/bin/apktool.jar
+curl -sL "https://githubusercontent.com" -o $HOME/bin/apktool
+curl -sL "https://github.com" -o $HOME/bin/apktool.jar
 chmod +x $HOME/bin/apktool $HOME/bin/apktool.jar
 
 # 4. Instal dependensi Python via pip
